d29648986c11b64e374635f4786600774567623a
csrafsan <rafsanahmedbd21@gmail.com>
2022-07-15 09:33:15 +0600

    for if else endif endfor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 
-    function ShowHome(){
-
-            $NewHeadLine="<h1>Bangladesh won by 100 rune</h1>";
-            return view('Home',['NewHeadLineKey'=>$NewHeadLine]);
-    }
+   
     function HomeClass(){
 
         return view('Home');
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,8 +1,9 @@
-{!!$NewHeadLineKey!!}
+@for ($i = 0; $i < 10; $i++)
+    @if($i%2==0)
+    <button >{{ $i }}</button>
+    @else
+    <a herf="#" >{{ $i }}</a>
+    @endif
 
 
-{{$NewHeadLineKey}}
-
-
-
- <h1>This is the example of blade view</h1>
+@endfor
