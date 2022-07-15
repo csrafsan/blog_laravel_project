9d12e85d4b45f3993b6496de5c52e3ef63ab234e
csrafsan <rafsanahmedbd21@gmail.com>
2022-07-15 11:35:30 +0600

    Php inside blade view Part 30

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,24 +1,8 @@
-@foreach($DataKey as $CountryName)
+@php
+$num1=4;
+$num2=9;
 
- <p>{{$loop->index}}. Country Name: {{$CountryName}}</p>
-
-@endforeach
-
-
-<!-- <select>
-@forelse($DataKey as $CountryName)
-<option value="">{{$CountryName}}</option> 
-@empty
-<option value=""><p>No country</p></option>
-@endforelse
-</select>
-
-@if($LogInStatusKey==true)
-<p>You have successfully logged In</p>
-@elseif($LogInStatusKey==false)
-<p>You are not logged In</p>
-@else
-<p>LogIn status not found</p>
-@endif -->
-
-
+$result =  $num1+$num2;
+echo $result."<br>";
+echo time();
+@endphp
