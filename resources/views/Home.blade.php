914bf95304e3ef024933216892593d6641653edf
csrafsan <rafsanahmedbd21@gmail.com>
2022-07-15 12:35:16 +0600

    include first

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,15 +1,1 @@
-@include('subview.Menu')
-<br>
-<br>
-<br>
-<br>
-
-<h1>this is my home page</h1>
-
-<br>
-<br>
-<br>
-<br>
-
-@include('subview.Footer',['footerText'=>'All Rights Reserved By Rafsan'])
-
+@includefirst(['subview.Footer','subview.Menu'])
